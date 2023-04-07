89e0ac914021357740cd2ba39b5a75fd1427ea3b
Souravendra Krishna Deb <souravendra.official@gmail.com>
2023-04-07 14:54:28 +0530

    generated nodejs template for backend (i gave up trying to write my own middleware, it would take way too long)

---
+++ backend/src/config/database.js
@@ -0,0 +1,15 @@
+require('dotenv/config');
+
+module.exports = {
+  dialect: process.env.DB_DIALECT,
+  dialectOptions: {
+    ssl: { require: 'true' },
+  },
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+  },
+};
