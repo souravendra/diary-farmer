34363a5ad0eec5a1dc690c964ec55093a1d32c09
Souravendra Krishna Deb <souravendra.official@gmail.com>
2023-04-08 10:26:25 +0530

    added service to do something cool later on currently has open in new tab functionality

---
+++ ui/services/url.service.js
@@ -0,0 +1,12 @@
+const ApiUrl = process.env.NEXT_PUBLIC_API_URL;
+const UIbaseURL = NEXT_PUBLIC_UI_URL;
+const S3_BASE_URL = NEXT_PUBLIC_S3_BASE_URL;
+
+export const openInNewTab = (url) => {
+  const newWindow = window.open(
+    UIbaseURL + url,
+    '_blank',
+    'noopener,noreferrer',
+  );
+  if (newWindow) newWindow.opener = null;
+};
