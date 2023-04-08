a9c73bed1dbb96cc637a610cf40e6b62c116546d
Souravendra Krishna Deb <souravendra.official@gmail.com>
2023-04-08 12:49:15 +0530

    added login route, updated dependency

--- backend/src/routes/login.routes.js
+++ backend/src/routes/login.routes.js
@@ -1,11 +1,12 @@
-import { Router } from "express";
+import { Router } from 'express';
 
-import loginController from "../controllers/login.controller";
-import authMiddleware from "../middlewares/auth.middleware";
+import loginController from '../controllers/login.controller';
+import authMiddleware from '../middlewares/auth.middleware';
 
 const loginRoutes = Router();
 
-loginRoutes.post("/login", loginController.login);
-loginRoutes.get("/logout", authMiddleware, loginController.logout);
+loginRoutes.post('/login', loginController.login);
+loginRoutes.get('/logout', authMiddleware, loginController.logout);
+loginRoutes.get('/test', loginController.test);
 
 export { loginRoutes };
