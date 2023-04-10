b0d39167e1f62c17fba0c4e8e689a652f4f27544
Souravendra Krishna Deb <souravendra.official@gmail.com>
2023-04-10 08:09:58 +0530

    disabled server side rending on _app.js to resolve react hydration error

--- ui/pages/_app.js
+++ ui/pages/_app.js
@@ -1,5 +1,26 @@
-import '@/styles/globals.css'
+/* eslint-disable react/prop-types */
+/* eslint-disable react/react-in-jsx-scope */
+import '@/styles/globals.css';
+import { appWithTranslation } from 'next-i18next';
+import nextI18nextConfig from '@/next-i18next.config';
+import WithAuth from '@/components/auth/WithAuth.js';
+import { getLayout } from '@/components/layout/LayoutConfiguration';
+import dynamic from 'next/dynamic';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const DynamicAuthWithNoSSR = dynamic(
+  () => import('@/components/auth/WithAuth.js'),
+  {
+    ssr: false,
+  },
+);
+
+function MyApp({ Component, pageProps, router }) {
+  let layout = getLayout(router.pathname);
+  return (
+    <DynamicAuthWithNoSSR router={router}>
+      {layout({ children: <Component {...pageProps} /> })}
+    </DynamicAuthWithNoSSR>
+  );
 }
+
+export default appWithTranslation(MyApp, nextI18nextConfig);
